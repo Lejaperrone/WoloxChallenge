799c78b7ad4adbd8ebcf64588878407bdadff0cc
Alejo <lejaperrone@gmail.com>
2019-07-06 19:04:33 -0300

    Avances en user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -16,7 +16,7 @@ class UserController extends AbstractFOSRestController
     /**
      * @Rest\Get("/users")
      */
-    public function getUserAction()
+    public function getUsersAction()
     {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $users = $repository->findAll();
@@ -24,15 +24,81 @@ class UserController extends AbstractFOSRestController
         return $this->handleView($this->view($view));
     }
 
-    public function editUserAction()
+    /**
+     * @Rest\Get("/users/{id}")
+     */
+    public function getUserAction($id)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+        $view = $this->view($user, Response::HTTP_OK);
+        return $this->handleView($this->view($view));
     }
 
-    public function deleteUserAction(Request $request)
+    /**
+     * @Rest\Put("/users/{id}")
+     */
+    public function editUserAction(Request $request, $id)
     {
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $image = $request->get('image');
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+
+        $user->setName($name);
+        $user->setEmail($email);
+        $user->setImage($image);
+
+        $em->persist($user);
+        $em->flush();
+
+        $view = $this->view($user, Response::HTTP_OK);
+        return $this->handleView($this->view($view));
     }
 
-    public function createUserAction()
+    /**
+     * @Rest\Delete("/users/{id}")
+     */
+    public function deleteUserAction($id)
     {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        //TODO Que devuelvo?
+        $view = $this->view(null, Response::HTTP_OK);
+        return $this->handleView($this->view($view));
+    }
+
+
+    //Headers Content-Type application/json
+    //Body {"name": "Alejo", "email": "lejaperrone@gmail.com", "image":"imagen"}
+    /**
+     * @Rest\Post("/users")
+     */
+    public function createUserAction(Request $request)
+    {
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $image = $request->get('image');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = New User();
+        $user->setName($name);
+        $user->setEmail($email);
+        $user->setImage($image);
+
+        $em->persist($user);
+        $em->flush();
+
+        $view = $this->view($user, Response::HTTP_OK);
+        return $this->handleView($this->view($view));
     }
 }
